7cd8cb50e590ab58c36538b497c16cb6bd8f7379
M. Ali Erdoğan <muhammetalie19@gmail.com>
2022-11-20 14:28:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from pytube import YouTube
 import time as tim
+import os
 
 app = Flask(__name__)
 
@@ -11,96 +12,122 @@ def homepage():
 
 @app.route("/youtube/api", methods = ['POST', 'GET'])
 def youtube():
-    global file_type
-    global link_input
-    global title
-    global video1
-    global video2
     
-    if request.method == "POST":
-        file_type = request.form.get("file_type")
-        link_input = request.form.get("link_input")
+    try:
     
-        video = YouTube(link_input)
+        global file_type
+        global link_input
+        global title
+        global video1
+        global video2
+        
+        if request.method == "POST":
+            file_type = request.form.get("file_type")
+            link_input = request.form.get("link_input")
+        
+            video = YouTube(link_input)
+        
+            thumbnail_url = YouTube(link_input).thumbnail_url
+            title = YouTube(link_input).title
+            description = YouTube(link_input).description
+            author = YouTube(link_input).author
+            length = tim.strftime("%H:%M:%S", tim.gmtime(YouTube(link_input).length))
+            time = str(YouTube(link_input).publish_date)
+            
+            
+            videos = video.streams.filter(progressive=True)
+            
+            video1 = videos[1]
+            video2 = videos[2]
+            
+            video1_filesize = int(video1.filesize/1048576)
+            video2_filesize = int(video2.filesize/1048576)
+            
+            
+            
+            if file_type == "mp3":
+                return render_template("index2.html",
+                thumbnail_url = thumbnail_url,
+                title = title,
+                description = description[:300],
+                author = author,
+                time = time[:10],
+                length = length,)
+            
+            
+            return render_template("index1.html",
+                thumbnail_url = thumbnail_url,
+                title = title,
+                description = description[:300],
+                author = author,
+                time = time[:10],
+                length = length,
+                video1 = video1,
+                video2 = video2,
+                video1_filesize = video1_filesize,
+                video2_filesize = video2_filesize)
     
-        thumbnail_url = YouTube(link_input).thumbnail_url
-        title = YouTube(link_input).title
-        description = YouTube(link_input).description
-        author = YouTube(link_input).author
-        length = tim.strftime("%H:%M:%S", tim.gmtime(YouTube(link_input).length))
-        time = str(YouTube(link_input).publish_date)
-        
-        
-        videos = video.streams.filter(progressive=True)
-        
-        video1 = videos[1]
-        video2 = videos[2]
-        
-        video1_filesize = int(video1.filesize/1048576)
-        video2_filesize = int(video2.filesize/1048576)
-        
-        
-        
-        if file_type == "mp3":
-            return render_template("index2.html",
-            thumbnail_url = thumbnail_url,
-            title = title,
-            description = description[:300],
-            author = author,
-            time = time[:10],
-            length = length,)
-        
-        
-        return render_template("index1.html",
-            thumbnail_url = thumbnail_url,
-            title = title,
-            description = description[:300],
-            author = author,
-            time = time[:10],
-            length = length,
-            video1 = video1,
-            video2 = video2,
-            video1_filesize = video1_filesize,
-            video2_filesize = video2_filesize)
+    except:
+        return render_template("error.html")
     
-    else:
-        return redirect(url_for("homepage"))
 
 
 @app.route("/downloadmp3", methods = ['POST', 'GET'])
 def downloadmp3():
-    global file_type
-    global link_input
-    global title
-        
-    video = YouTube(link_input).streams.filter(only_audio=True).first()
-    video.download(filename=title+".mp3")
+    try:
+        global file_type
+        global link_input
+        global title
+            
+        video = YouTube(link_input).streams.filter(only_audio=True).first()
+        video = video.download(filename=title+".mp3")
 
-    return render_template("downloaded.html")
+        return send_file(video, as_attachment=True)
+    
+    except:
+        return render_template("error.html")
+    
+    finally:
+        os.remove(video)
+
 
 @app.route("/download360p", methods = ['POST', 'GET'])
 def download360p():
-    global file_type
-    global link_input
-    global title
-    global video1
+    try:
+        global file_type
+        global link_input
+        global title
+        
+        video = video1.download(filename=title+"_360p"+".mp4")
+        return send_file(video, as_attachment=True, download_name=title+"_360p", mimetype="video/mp4")
     
-    video1.download(filename=title+"_360p"+".mp4")
-
-    return render_template("downloaded.html")
+    except:
+        return render_template("error.html")
+    
+    finally:
+        os.remove(video)
+        
+        
 
 @app.route("/download720p", methods = ['POST', 'GET'])
 def download720p():
-    global file_type
-    global link_input
-    global title
-    global video2
+    try:
+        global file_type
+        global link_input
+        global title
+        global video2
+        
+        video = video2.download(filename=title+"_720p"+".mp4")
+        return send_file(video, as_attachment=True, download_name=title+"_720p", mimetype="video/mp4")
     
-    video2.download(filename=title+"_720p"+".mp4")
+    except:
+        return render_template("error.html")
     
-    return render_template("downloaded.html")
-
-
+    finally:
+        os.remove(video)
+        
+        
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
